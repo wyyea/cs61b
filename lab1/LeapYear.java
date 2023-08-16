0425a5d7cbcb78671c32b0178ec7b29422824ddb
wyyea <2639617875@qq.com>
2023-08-17 00:39:06 +0800

    Lab 1 finish

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,10 +1,27 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ * 
+ * @author YOUR NAME HERE
  */
 public class LeapYear {
+    /**
+     * judge if a year is a leap year
+     * 
+     * @param year
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0)
+            return true;
+        if (year % 4 == 0)
+            if (year % 100 != 0)
+                return true;
+        return false;
+    }
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
+    /**
+     * Calls isLeapYear to print correct statement.
+     * 
+     * @param year to be analyzed
      */
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
@@ -30,4 +47,3 @@ public class LeapYear {
         }
     }
 }
-
