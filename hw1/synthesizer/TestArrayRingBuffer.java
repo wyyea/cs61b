c21e52ebea257eece7390e2fb32b7120c1f729a5
wyyea <2639617875@qq.com>
2023-09-05 20:06:31 +0800

    hw1 finish

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,19 +1,41 @@
 package synthesizer;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Iterator;
 
-/** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
+/**
+ * Tests the ArrayRingBuffer class.
+ * 
+ * @author Josh Hug
  */
 
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(4);
+        assertTrue(arb.isEmpty());
+        assertFalse(arb.isFull());
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+        assertFalse(arb.isEmpty());
+        assertTrue(arb.isFull());
+        for (Integer i : arb) {
+            for (Integer j : arb) {
+                System.out.println("i: " + i + " j: " + j);
+            }
+        }
+        for (int i = 1; i <= 4; i++) {
+            assertTrue(arb.peek() == i);
+            assertTrue(arb.dequeue() == i);
+        }
+
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
     }
-} 
+}
