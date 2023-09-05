855f91e35d6d2dfedb281b6d267d987a947c62a0
wyyea <2639617875@qq.com>
2023-09-05 09:24:59 +0800

    proj1b fix

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -5,18 +5,18 @@ public class TestOffByOne {
     /*
      * // You must use this CharacterComparator and not instantiate
      * // new ones, or the autograder might be upset.
-     * static CharacterComparator offByOne = new OffByOne();
      * 
      * // Your tests go here.
      * Uncomment this class once you've created your CharacterComparator interface
      * and OffByOne class.
      **/
+    static CharacterComparator offByOne = new OffByOne();
+
     @Test
     public void testequalChars() {
-        OffByOne obo = new OffByOne();
-        assertTrue(obo.equalChars('a', 'b'));
-        assertTrue(obo.equalChars('e', 'd'));
-        assertFalse(obo.equalChars('a', 'a'));
-        assertFalse(obo.equalChars('z', 'a'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('e', 'd'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('z', 'a'));
     }
 }
